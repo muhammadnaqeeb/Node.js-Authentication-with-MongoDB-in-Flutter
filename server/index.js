8493d23fab28011c87d6c5dee2de7f15462dcc8b
mnaqeeb06 <muhammadnaqeebawan@gmail.com>
2023-02-09 10:27:05 +0500

    Creating Simple Server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-console.log("Hello Again")
+// importing
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+// initillize express into a variable
+const app = express();
+
+// middleware, this basically make shore that whatever request 
+// which is coming to our server get passed in json format
+app.use(express.json());
+
+// listening to port
+// 0.0.0.0 means accessable from anywhere
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`connected at port ${PORT}`);
+});
