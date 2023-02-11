5696fcd9842f294a0507db4a1e451bc383412b17
mnaqeeb06 <muhammadnaqeebawan@gmail.com>
2023-02-11 16:35:55 +0500

    Showing user data on Home Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/user_provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<UserProvider>(context).user;
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: const [Text("Yes")],
+        children: [
+          Text(user.name),
+          Text(user.email),
+          Text(user.id),
+          //Text(user.password)
+        ],
       ),
     );
   }
